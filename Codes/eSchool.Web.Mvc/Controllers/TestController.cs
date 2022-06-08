23694368f4a0f8309101d48f187b86b023283785
QuocBinh <kelvinbinh.sd@gmail.com>
2022-06-08 15:54:42 +0700

    Add Abp code demo

---
+++ Codes/eSchool.Web.Mvc/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using eSchool.Controllers;
+using eSchool.UseCases.Branches;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace eSchool.Web.Controllers
+{
+    public class TestController : eSchoolControllerBase
+    {
+        private readonly IBranchAppService _branchAppService;
+
+        public TestController(IBranchAppService branchAppService)
+        {
+            _branchAppService = branchAppService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            await _branchAppService.GetAllAsync(new Abp.Application.Services.Dto.PagedAndSortedResultRequestDto());
+            return View();
+        }
+    }
+}
